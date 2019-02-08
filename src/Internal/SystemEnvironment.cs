759300b7287b1de87c4c5dba01fe55cb7f574b66
Zac Charles <zac.charles@gmail.com>
2019-02-08 14:07:15 +0000

    Implement workaround for setting env vars

--- src/Internal/SystemEnvironment.cs
+++ src/Internal/SystemEnvironment.cs
@@ -1,13 +1,20 @@
 ﻿using System;
 using System.Collections;
+using System.Runtime.InteropServices;
 
 namespace LambdaNative.Internal
 {
     internal class SystemEnvironment : IEnvironment
     {
+        [DllImport("c")]
+        private static extern void setenv(string key, string value);
+
         public void SetEnvironmentVariable(string key, string value)
         {
-            Environment.SetEnvironmentVariable(key, value);
+            // Workaround because Environment.SetEnvironmentVariable is not implemented in CoreRT.
+            // See https://github.com/dotnet/corert/issues/6971 for more details.
+            try { Environment.SetEnvironmentVariable(key, value); }
+            catch { setenv(key, value); }
         }
 
         public IDictionary GetEnvironmentVariables()
