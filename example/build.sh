136574a0d2958161510ab370d362a17292ee5795
iFew <chitpong@gmail.com>
2019-04-24 15:16:54 +0700

    Added script build by Docker and rollback build by script

--- example/build.sh
+++ example/build.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 
-#
-# SHOULD BUILD IMAGE BEFORE
-# docker build -t lambdanative .
-#
-
-rm -f $(pwd)/bootstrap
-rm -f $(pwd)/package.zip
-docker run --rm -v $(pwd):/app lambdanative
-cp bin/Release/netcoreapp2.1/linux-x64/native/LambdaNative.Example bootstrap
-zip package.zip bootstrap
-# aws s3 cp package.zip s3://backend-layer [--profile [PROFILE-NAME]]
+dotnet publish -r linux-x64 -c release
+cp bin/release/netcoreapp*/linux-x64/native/* bootstrap
+# zip package.zip bootstrap
+# aws s3 cp package.zip s3://<bucket>/package.zip
